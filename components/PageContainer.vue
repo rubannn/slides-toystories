8ecbe19735f15163e437e6b44454570af4072a88
Serhii Sydorenko <sidorchik91@gmail.com>
2026-09-25 19:29:50 +0200

    Add changes to 01.md and PageContainer

--- components/PageContainer.vue
+++ components/PageContainer.vue
@@ -10,10 +10,15 @@
 .page-container {
   position: absolute;
   width: 100%;
-  height: 100%;
+  height: calc(100% - 120px);
   top: 120px;
   left: 0;
   padding: 40px;
   background-color: cornsilk;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #17494b;
 }
 </style>
